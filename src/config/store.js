61a634321f8cfcf01cb4b4d2de4321ca0a3f4ade
juliecz <yuliya@yuliya.cz>
2018-08-04 15:54:29 +0200

    Update bootstrap, hide logger for production environment

--- src/config/store.js
+++ src/config/store.js
@@ -1,15 +1,26 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+import { createLogger } from 'redux-logger';
 import rootReducer from '../reducers/index';
 import { browserHistory } from 'react-router';
 import { routerMiddleware } from 'react-router-redux';
 
-const composeEnhancers = (process.env.NODE_ENV !== 'production' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+const DEBUG = process.env.NODE_ENV !== 'production';
+
+const composeEnhancers =
+	(DEBUG && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+
+const logger = createLogger({
+	predicate: (getState, action) => DEBUG
+});
 
 const store = createStore(
 	rootReducer,
-	composeEnhancers(applyMiddleware(routerMiddleware(browserHistory), thunk, logger))
+	composeEnhancers(
+		applyMiddleware(routerMiddleware(browserHistory),
+		thunk,
+		logger
+	))
 );
 
 export default store;
